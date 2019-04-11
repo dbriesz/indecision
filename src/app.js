eec043086ae47f4c5727512d5e6c1b42f8a71632
David Riesz <dbriesz@users.noreply.github.com>
2019-04-11 15:18:06 -0700

    Move Option component to separate file

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AddOption from './components/AddOption';
+import Option from './components/Option';
 
 class IndecisionApp extends React.Component {
     constructor(props) {
@@ -122,19 +123,6 @@ class IndecisionApp extends React.Component {
             />
           ))
         }
-      </div>
-    )
-  
-  const Option = (props) => (
-      <div>
-        {props.optionText}
-        <button 
-          onClick={(e) => {
-            props.handleDeleteOption(props.optionText);
-          }}
-          >
-          remove
-        </button>
       </div>
     )
   
